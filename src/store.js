c5a6ac7820afb0fd49de68cff96a8e56d6ba2085
giuseppesciacca <sciaccagi@gmail.com>
2023-06-13 16:47:56 +0200

    ADD callApi to projects

---
+++ src/store.js
@@ -0,0 +1,23 @@
+// store.js
+import { reactive } from 'vue'
+import axios from 'axios'
+
+
+export const store = reactive({
+    urlProjectsApi: 'http://127.0.0.1:8000/api/projects',
+
+    /**
+     * 
+     * @param {string} url 
+     */
+    fetchProjects(url) {
+        axios
+            .get(url)
+            .then(response => {
+                //console.log(response.data);
+                console.log(response.data.projects);
+            }).catch(error => {
+                console.error(error.message);
+            })
+    },
+})
