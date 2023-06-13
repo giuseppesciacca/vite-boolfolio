b1d339c5b7511d6b484bb08ecff0ad17d9feb29b
giuseppesciacca <sciaccagi@gmail.com>
2023-06-13 17:02:37 +0200

    ADD card and print projects on page

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,26 @@ export default {
     <!-- /#app_header -->
     <main id="app_main">
         <div class="container">
-            <div class="row row-cols-2 row-cols-md-4 row-cols-lg-6">
+            <div class="row row-cols-2 row-cols-md-4 row-cols-lg-6 gap-3">
+                <div v-for="item in        store.projects       " class="col">
+                    <div class="card h-100">
+                        <img class="card-img-top" :alt="item.slug">
+                        <div class="card-body">
+                            <h4 class="card-title">{{ item.title }}</h4>
+                            <div v-if="item.description">
+                                <p class="card-text">{{ item.description }}</p>
+                            </div>
+                            <div v-if="item.tecnologies" v-for="tecnology in item.tecnologies">
+                                <span class="badge bg-primary">{{ tecnology.name }}</span>
+                            </div>
+                        </div>
+                        <div class="card-footer text-muted">
+                            <div>
+                                <span class="badge bg-secondary">{{ item.type.name }}</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
             </div>
         </div>
--- src/store.js
+++ src/store.js
@@ -5,6 +5,7 @@ import axios from 'axios'
 
 export const store = reactive({
     urlProjectsApi: 'http://127.0.0.1:8000/api/projects',
+    projects: [],
 
     /**
      * 
@@ -16,6 +17,7 @@ export const store = reactive({
             .then(response => {
                 //console.log(response.data);
                 console.log(response.data.projects);
+                this.projects = response.data.projects;
             }).catch(error => {
                 console.error(error.message);
             })
