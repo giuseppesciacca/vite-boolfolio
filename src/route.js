f8ccba7d06da596f62cc110ebba05a2c171708c2
giuseppesciacca <sciaccagi@gmail.com>
2023-06-14 15:50:31 +0200

    ADD vue-router + route.js + /pages/...

---
+++ src/route.js
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+// 1. Define route components.
+// These can be imported from other files
+import AppHome from './pages/AppHome.vue';
+import AppAbout from './pages/AppAbout.vue';
+import AppContact from './pages/AppContact.vue';
+
+// 2. Define some routes
+// Each route should map to a component.
+const routes = [
+    { path: '/', name: 'home', component: AppHome },
+    { path: '/about', name: 'about', component: AppAbout },
+    { path: '/contacts', name: 'contacts', component: AppContact },
+]
+
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here
+const router = createRouter({
+    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+    history: createWebHistory(),
+    routes, // short for `routes: routes`
+});
+
+// 5. Export router.
+export { router }
