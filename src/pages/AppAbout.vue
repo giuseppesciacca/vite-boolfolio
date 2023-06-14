8809cb73933ddbadeab926655c780baec9f056a7
giuseppesciacca <sciaccagi@gmail.com>
2023-06-14 16:30:07 +0200

    ADD About view

--- src/pages/AppAbout.vue
+++ src/pages/AppAbout.vue
@@ -5,8 +5,28 @@ export default {
 </script>
 
 <template>
-    <div>
-
+    <div class="container py-5">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio tempora atque accusantium delectus hic
+        dignissimos facere quos quia, officia aspernatur sit nemo facilis? Porro perferendis ullam fugit magni libero
+        pariatur?
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio tempora atque accusantium delectus hic
+        dignissimos facere quos quia, officia aspernatur sit nemo facilis? Porro perferendis ullam fugit magni libero
+        pariatur? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio tempora atque accusantium delectus
+        hic
+        dignissimos facere quos quia, officia aspernatur sit nemo facilis? Porro perferendis ullam fugit magni libero
+        pariatur? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio tempora atque accusantium delectus
+        hic
+        dignissimos facere quos quia, officia aspernatur sit nemo facilis? Porro perferendis ullam fugit magni libero
+        pariatur? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio tempora atque accusantium delectus
+        hic
+        dignissimos facere quos quia, officia aspernatur sit nemo facilis? Porro perferendis ullam fugit magni libero
+        pariatur? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio tempora atque accusantium delectus
+        hic
+        dignissimos facere quos quia, officia aspernatur sit nemo facilis? Porro perferendis ullam fugit magni libero
+        pariatur? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio tempora atque accusantium delectus
+        hic
+        dignissimos facere quos quia, officia aspernatur sit nemo facilis? Porro perferendis ullam fugit magni libero
+        pariatur?
     </div>
 </template>
 
