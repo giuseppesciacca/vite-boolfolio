6b6213022053ac9d96c186c50ee817e7d8a0b4b1
giuseppesciacca <sciaccagi@gmail.com>
2023-06-15 17:37:01 +0200

    ADD link + fix in footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -10,17 +10,17 @@ export default {
 
             <ul>
                 <li>
-                    <router-link class="nav-link text-white" aria-current="page" :to="{ name: 'home' }">
+                    <router-link class="nav-link text-white d-inline" aria-current="page" :to="{ name: 'home' }">
                         Home
                     </router-link>
                 </li>
                 <li>
-                    <router-link class="nav-link text-white" aria-current="page" :to="{ name: 'about' }">
+                    <router-link class="nav-link text-white d-inline" aria-current="page" :to="{ name: 'about' }">
                         About
                     </router-link>
                 </li>
                 <li>
-                    <router-link class="nav-link text-white" aria-current="page" :to="{ name: 'contacts' }">
+                    <router-link class="nav-link text-white d-inline" aria-current="page" :to="{ name: 'contacts' }">
                         Contacts
                     </router-link>
                 </li>
@@ -30,18 +30,13 @@ export default {
                 <div class="col-6">
                     <p>
                         Made by PEPS
+                        <a href="https://github.com/giuseppesciacca" class="me-2">
+                            <font-awesome-icon :icon="['fab', 'github']" size="2xl" />
+                        </a>
+                        <a href="https://www.linkedin.com/in/giuseppe-s-b43079167" class="me-2">
+                            <font-awesome-icon :icon="['fab', 'linkedin']" size="2xl" />
+                        </a>
                     </p>
-                </div>
-                <div class="col-6">
-                    <a href="#" class="me-2">
-                        <font-awesome-icon :icon="['fab', 'twitter']" size="2xl" />
-                    </a>
-                    <a href="#" class="me-2">
-                        <font-awesome-icon :icon="['fab', 'linkedin']" size="2xl" />
-                    </a>
-                    <a href="#">
-                        <font-awesome-icon :icon="['fab', 'github']" size="2xl" />
-                    </a>
                 </div>
             </div>
 
