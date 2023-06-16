d30232e0d6d3ad37f7ffe9df14cc8a8f9aacb82a
giuseppesciacca <sciaccagi@gmail.com>
2023-06-16 17:27:13 +0200

    EDIT footer layouts

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -5,43 +5,69 @@ export default {
 </script>
 
 <template>
-    <footer id="app_footer" class="bg-dark py-3 text-white">
+    <footer id="app_footer" class="py-3 text-white">
         <div class="container">
-
-            <ul>
-                <li>
-                    <router-link class="nav-link text-white d-inline" aria-current="page" :to="{ name: 'home' }">
-                        Home
-                    </router-link>
-                </li>
-                <li>
-                    <router-link class="nav-link text-white d-inline" aria-current="page" :to="{ name: 'about' }">
-                        About
-                    </router-link>
-                </li>
-                <li>
-                    <router-link class="nav-link text-white d-inline" aria-current="page" :to="{ name: 'contacts' }">
-                        Contacts
-                    </router-link>
-                </li>
-            </ul>
-
             <div class="row">
-                <div class="col-6">
-                    <p>
-                        Made by PEPS
-                        <a href="https://github.com/giuseppesciacca" class="me-2">
-                            <font-awesome-icon :icon="['fab', 'github']" size="2xl" />
-                        </a>
-                        <a href="https://www.linkedin.com/in/giuseppe-s-b43079167" class="me-2">
-                            <font-awesome-icon :icon="['fab', 'linkedin']" size="2xl" />
-                        </a>
-                    </p>
+                <div class="col-8 col-lg-9">
+                    <h4>Links</h4>
+
+                    <ul>
+                        <li>
+                            <router-link class="nav-link text-white d-inline" aria-current="page" :to="{ name: 'home' }">
+                                Home
+                            </router-link>
+                        </li>
+                        <li>
+                            <router-link class="nav-link text-white d-inline" aria-current="page" :to="{ name: 'about' }">
+                                About
+                            </router-link>
+                        </li>
+                        <li>
+                            <router-link class="nav-link text-white d-inline" aria-current="page"
+                                :to="{ name: 'contacts' }">
+                                Contact me
+                            </router-link>
+                        </li>
+                    </ul>
+                    <!-- links -->
+
+                </div>
+
+                <div class="col-4 col-lg-3">
+                    <h4>Social</h4>
+
+                    <a href="https://github.com/giuseppesciacca" class="me-2 github">
+                        <font-awesome-icon :icon="['fab', 'github']" size="xl" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/giuseppe-s-b43079167" class="me-2 linkedin">
+                        <font-awesome-icon :icon="['fab', 'linkedin']" size="xl" />
+                    </a>
                 </div>
             </div>
 
+            <hr>
+
+            <p class="m-0">
+                © Copyright 2023. Made by Giuseppe Sciacca
+            </p>
+            <!-- credits -->
         </div>
+
     </footer>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+footer {
+    font-family: 'Source Code Pro', monospace;
+    min-height: 200px;
+    background-color: black;
+}
+
+.github {
+    color: white;
+}
+
+.linkedin {
+    color: #0961b8;
+}
+</style>
